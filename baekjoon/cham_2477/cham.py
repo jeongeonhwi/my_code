f735249d7173f28cff5d08b4a312200785144ab5
전건휘 <36934wkd@github.com>
2023-08-21 17:08:38 +0900

    8-21

--- baekjoon/cham_2477/cham.py
+++ baekjoon/cham_2477/cham.py
@@ -3,32 +3,19 @@ sys.stdin = open('input.txt')
 
 
 K = int(input())
-double = []
+num = []
 arr = []
 for _ in range(6):
     a, b = map(int, input().split())
-    if a in arr:
-        double.append(a)
-        double.append(b)
-    else:
-        arr.append(a)
-        arr.append(b)
-small = []
-for i in range(len(double)//2):
-    for j in range(len(arr)//2):
-        if double[i*2] == arr[j*2]:
-            if double[i*2+1] > arr[j*2+1]:
-                small.append(arr[j*2])
-                small.append(arr[j*2+1])
-            else:
-                small.append(double[i*2])
-                small.append(double[i*2+1])
-arr2 = double + arr
-max_v = 0
-max_idx = 0
-for i in range(len(small)//2):
-    for j in range(len(arr2)//2):
-        if small[i*2] == arr2[j*2]:
-            arr2[j*2] = 0
-            arr2[j*2+1] = 0
-print(arr2)
+    num.append(a)
+    arr.append(b)
+min_idx = 0
+for i in range(len(num)):
+    if num[i%6] == num[(i+2)%6] and num[(i+1)%6] == num[(i+3)%6]:
+        min_idx = i
+        break
+min_v = arr[(min_idx+1)%6]*arr[(min_idx+2)%6]
+max_v = arr[(min_idx+4)%6]*arr[(min_idx+5)%6]
+
+
+print((max_v-min_v)*K)
