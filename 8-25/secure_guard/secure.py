d6718208798f725a17d79deaf16f31ae646b19e6
hello1334 <hello1334>
2023-08-26 23:01:23 +0900

    8-26secure

--- 8-25/secure_guard/secure.py
+++ 8-25/secure_guard/secure.py
@@ -8,7 +8,7 @@ for tc in range(1, T+1):
     max_house = 0
     for i in arr:
         max_house += i.count(1)
-    k = 1
+    k = 0
     while True:
         if k*k+(k-1)*(k-1) > max_house*M:
             k -= 1
@@ -17,24 +17,28 @@ for tc in range(1, T+1):
     total = 0
     for i in range(N):
         for j in range(N):
-            for kk in range(1, k):
+            for kk in range(1, k+1):
                 srt_i = i-kk+1
                 srt_j = j-kk+1
-                print(srt_i, srt_j)
-                if 0<=srt_i<N and 0<=srt_j<N:
-                    count = 0
-                    for ii in range(kk+1):
-                        for jj in range(kk+1):
-                            ni = srt_i+ii
-                            nj = srt_j+jj
-                            if 0<=ni<N and 0<=nj<N:
-                                if abs(i - ni) + abs(j - nj) <= kk-1:
-                                    if arr[ni][nj] == 1:
-                                        count += 1
-                    if kk*kk+(kk-1)*(kk-1) <= count*M:
-                        # print(count)
-                        if total <= count:
-                            total = count
-    print(total)
-    print(f'#{tc}')
+                count = 0
+                for ii in range(kk+kk-1):
+                    for jj in range(kk+kk-1):
+                        ni = srt_i+ii
+                        nj = srt_j+jj
+                        if 0<=ni<N and 0<=nj<N:
+                            # print(ni, nj)
+                            if abs(i - ni) + abs(j - nj) < kk:
+                                if arr[ni][nj] == 1:
+                                    count += 1
+                            # if abs(i - ni) + abs(j - nj) < kk:
+                            #     if i == 3 and j == 3 and kk == 4:
+                            #         arr[ni][nj] += 1
+                            #         count += 1
+                # print(count)
+                if kk*kk+(kk-1)*(kk-1) <= count*M:
+                    # print(count)
+                    if total <= count:
+                        total = count
+    # print(arr)
+    print(f'#{tc} {total}')
 
