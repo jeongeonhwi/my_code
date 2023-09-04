cb6a6d3cf95cb8eb7d1d57bbcc8a96ae068e4694
hello1334 <36934wkd@naver.com>
2023-09-04 21:23:11 +0900

    baekjoon continue

--- python/baekjoon/16637/16637.py
+++ python/baekjoon/16637/16637.py
@@ -1,12 +1,56 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+isp = {')':3, '*':2, '+':1, '-':1}
+
 N = int(input())
-arr = []
+stack = []
+s = []
 a_str = input()
+check = False
+checkplus = False
 for i in range(len(a_str)):
     if i%2==0:
-        arr.append(int(a_str[i]))
+        stack.append(int(a_str[i]))
     else:
-        arr.append(a_str[i])
-
+        if s:
+            if a_str[i] == '*' and s[-1] == '+':
+                tmp = s.pop()
+                s.append('(')
+                s.append(tmp)
+                s.append(')')
+                s.append(a_str[i])
+            elif a_str[i] == '+' and s[-1] == '*':
+                s.append('(')
+                s.append(a_str[i])
+                s.append(')')
+            elif a_str[i] == '-' and s[-1] == '-':
+                s.append('(')
+                s.append(a_str[i])
+                s.append(')')
+            else:
+                s.append(a_str[i])
+        else:      
+            s.append(a_str[i])
+susik = []
+idx = 0
+while s:
+    tmp = s.pop(0)
+    if tmp != '(':
+        if susik and susik[-1] != ')':
+            susik.append(stack.pop(0))
+            susik.append(tmp)
+            susik.append(stack.pop(0))
+            # 여기까지함
+    else:
+        a = susik.pop()
+        susik.append(tmp)
+        susik.append(a)
+        tmp = s.pop(0)
+        susik.append(tmp)
+        susik.append(stack.pop(0))
+        tmp = s.pop(0)
+        susik.append(tmp)
+        
+        
+        
