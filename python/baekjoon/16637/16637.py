a3acc279e4b2d193bec3658ffb08e19ef1f2d477
전건휘 <36934wkd@naver.com>
2023-09-05 16:43:02 +0900

    9-5

--- python/baekjoon/16637/16637.py
+++ python/baekjoon/16637/16637.py
@@ -1,8 +1,7 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-isp = {')':3, '*':2, '+':1, '-':1}
-
+isp = {')':3, '*':2, '+':1, '-':1, '(':0}
 N = int(input())
 stack = []
 s = []
@@ -32,6 +31,8 @@ for i in range(len(a_str)):
                 s.append(a_str[i])
         else:      
             s.append(a_str[i])
+print(s)
+print(stack)
 susik = []
 idx = 0
 while s:
@@ -39,7 +40,6 @@ while s:
     if tmp != '(':
         if susik:
             if susik[-1] != ')':
-                susik.append(stack.pop(0))
                 susik.append(tmp)
                 susik.append(stack.pop(0))
             else:
@@ -50,7 +50,7 @@ while s:
             susik.append(tmp)
             susik.append(stack.pop(0))
     else:
-        a = susik.pop()
+        a = stack.pop(0)
         susik.append(tmp)
         susik.append(a)
         tmp = s.pop(0)
@@ -58,8 +58,40 @@ while s:
         susik.append(stack.pop(0))
         tmp = s.pop(0)
         susik.append(tmp)
+print(susik)
 s = []
 stack = []
-while susik:
-    #여기까지
-    pass
+for i in susik:
+    if type(i) == int:
+        stack.append(i)
+    else:
+        if i == ')':
+            a = stack.pop()
+            b = stack.pop()
+            s1 = s.pop()
+            if s1 == '+':
+                s2 = a+b
+            elif s1 == '-':
+                s2 = a-b
+            else:
+                s2 = a*b
+            stack.append(s2)
+            s.pop()
+        else:
+            s.append(i)
+
+result = 0
+while s:
+    s1 = stack.pop(0)
+    s2 = stack.pop(0)
+    s3 = s.pop(0)
+    tmp = 0
+    if s3 == '+':
+        tmp = s1+s2
+    elif s3 == '-':
+        tmp = s1-s2
+    else:
+        tmp = s1*s2
+    stack.insert(0, tmp)
+result = stack[0]
+print(result)
