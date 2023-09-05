697e32e19252c3b3e093d80b5412fb9245ffc088
전건휘 <36934wkd@naver.com>
2023-09-05 17:55:59 +0900

    9-5

--- python/baekjoon/16637/16637.py
+++ python/baekjoon/16637/16637.py
@@ -1,97 +1,19 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-isp = {')':3, '*':2, '+':1, '-':1, '(':0}
-N = int(input())
-stack = []
-s = []
-a_str = input()
-check = False
-checkplus = False
-for i in range(len(a_str)):
-    if i%2==0:
-        stack.append(int(a_str[i]))
-    else:
-        if s:
-            if a_str[i] == '*' and s[-1] == '+':
-                tmp = s.pop()
-                s.append('(')
-                s.append(tmp)
-                s.append(')')
-                s.append(a_str[i])
-            elif a_str[i] == '+' and s[-1] == '*':
-                s.append('(')
-                s.append(a_str[i])
-                s.append(')')
-            elif a_str[i] == '-' and s[-1] == '-':
-                s.append('(')
-                s.append(a_str[i])
-                s.append(')')
-            else:
-                s.append(a_str[i])
-        else:      
-            s.append(a_str[i])
-print(s)
-print(stack)
-susik = []
-idx = 0
-while s:
-    tmp = s.pop(0)
-    if tmp != '(':
-        if susik:
-            if susik[-1] != ')':
-                susik.append(tmp)
-                susik.append(stack.pop(0))
-            else:
-                susik.append(tmp)
-                susik.append(stack.pop(0))
-        else:
-            susik.append(stack.pop(0))
-            susik.append(tmp)
-            susik.append(stack.pop(0))
-    else:
-        a = stack.pop(0)
-        susik.append(tmp)
-        susik.append(a)
-        tmp = s.pop(0)
-        susik.append(tmp)
-        susik.append(stack.pop(0))
-        tmp = s.pop(0)
-        susik.append(tmp)
-print(susik)
-s = []
-stack = []
-for i in susik:
-    if type(i) == int:
-        stack.append(i)
-    else:
-        if i == ')':
-            a = stack.pop()
-            b = stack.pop()
-            s1 = s.pop()
-            if s1 == '+':
-                s2 = a+b
-            elif s1 == '-':
-                s2 = a-b
-            else:
-                s2 = a*b
-            stack.append(s2)
-            s.pop()
-        else:
-            s.append(i)
+def dfs(i, value):
+    global max_v
+    if N == i:
+        max_v = max(max_v, int(value))
+        return
+    if i+4 <= N:
+        dfs(i+4, str(eval(''.join([value, arr[i]] + [str(eval(''.join(arr[i+1:i+4])))]))))
+    if i+2 <= N:
+        dfs(i+2, str(eval(''.join([value] + arr[i:i+2]))))
 
-result = 0
-while s:
-    s1 = stack.pop(0)
-    s2 = stack.pop(0)
-    s3 = s.pop(0)
-    tmp = 0
-    if s3 == '+':
-        tmp = s1+s2
-    elif s3 == '-':
-        tmp = s1-s2
-    else:
-        tmp = s1*s2
-    stack.insert(0, tmp)
-result = stack[0]
-print(result)
+
+N = int(input())
+arr = list(input())
+max_v = float('-inf')
+dfs(1, arr[0])
+print(max_v)
--- python/baekjoon/baseball_17281/17281.py
+++ python/baekjoon/baseball_17281/17281.py
@@ -1,119 +1,7 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-
-
-def sunyeol(i, eight):
-    global score
-    if i == eight:
-        nu = p[:]
-        tmparr = []
-        for i in range(len(arr)):
-            tmp = []
-            for n in range(10):
-                for nuu in range(len(nu)):
-                    if n == nu[nuu]:
-                        tmp.append(arr[i][nuu + 1])
-            tmp = tmp[:3] + [arr[i][0]] + tmp[3:]
-            tmparr.append(tmp)
-        outpoint = 0
-        current = 0
-        ru1 = False
-        ru2 = False
-        ru3 = False
-        ining = 0
-        tmpscore = 0
-        while ining < N:
-            if tmparr[ining][current % 9] == 0:
-                outpoint += 1
-                current += 1
-            elif tmparr[ining][current % 9] == 1:
-                if ru1:
-                    if ru2:
-                        if ru3:
-                            tmpscore += 1
-                        else:
-                            ru3 = True
-                    else:
-                        ru2 = True
-                        if ru3:
-                            tmpscore += 1
-                            ru3 = False
-                else:
-                    ru1 = True
-                    if ru2:
-                        if ru3:
-                            tmpscore += 1
-                            ru2 = False
-                        else:
-                            ru3 = True
-                            ru2 = False
-                    else:
-                        if ru3:
-                            tmpscore += 1
-                            ru3 = False
-                current += 1
-            elif tmparr[ining][current % 9] == 2:
-                if ru2:
-                    tmpscore += 1
-                else:
-                    ru2 = True
-                if ru3:
-                    tmpscore += 1
-                    if ru1:
-                        ru1 = False
-                    else:
-                        ru3 = False
-                else:
-                    if ru1:
-                        ru3 = True
-                        ru1 = False
-                current += 1
-            elif tmparr[ining][current % 9] == 3:
-                if ru1:
-                    tmpscore += 1
-                    ru1 = False
-                if ru2:
-                    tmpscore += 1
-                    ru2 = False
-                if ru3:
-                    tmpscore += 1
-                else:
-                    ru3 = True
-                current += 1
-            elif tmparr[ining][current % 9] == 4:
-                if ru1:
-                    tmpscore += 1
-                    ru1 = False
-                if ru2:
-                    tmpscore += 1
-                    ru2 = False
-                if ru3:
-                    tmpscore += 1
-                    ru3 = False
-                tmpscore += 1
-                current += 1
-            if outpoint >= 3:
-                ining += 1
-                outpoint = 0
-                if ru1:
-                    ru1 = False
-                if ru2:
-                    ru2 = False
-                if ru3:
-                    ru3 = False
-
-        if tmpscore > score:
-            score = tmpscore
-        return
-    else:
-        for j in range(eight):
-            if used[j] == 0:
-                p[i] = num[j]
-                used[j] = 1
-                sunyeol(i+1, eight)
-                used[j] = 0
-
-
+from itertools import permutations
+import copy
 # 0과 3번 인덱스는 교체한채 3번 인덱스 고정
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
@@ -122,7 +10,96 @@ num = [1, 2, 3, 4, 5, 6, 7, 8]
 eight = 8
 used = [0] * 8
 p = [0] * 8
-numbers = []
 score = 0
-sunyeol(0, eight)
+for p in permutations(num, eight):
+    p = list(p)
+    nu = p[:3] + [0] + p[3:]
+    outpoint = 0
+    current = 0
+    ru1 = False
+    ru2 = False
+    ru3 = False
+    ining = 0
+    tmpscore = 0
+    while ining < N:
+        if arr[ining][nu[current % 9]] == 0:
+            outpoint += 1
+            current += 1
+        elif arr[ining][nu[current % 9]] == 1:
+            if ru1:
+                if ru2:
+                    if ru3:
+                        tmpscore += 1
+                    else:
+                        ru3 = True
+                else:
+                    ru2 = True
+                    if ru3:
+                        tmpscore += 1
+                        ru3 = False
+            else:
+                ru1 = True
+                if ru2:
+                    if ru3:
+                        tmpscore += 1
+                        ru2 = False
+                    else:
+                        ru3 = True
+                        ru2 = False
+                else:
+                    if ru3:
+                        tmpscore += 1
+                        ru3 = False
+            current += 1
+        elif arr[ining][nu[current % 9]] == 2:
+            if ru2:
+                tmpscore += 1
+            else:
+                ru2 = True
+            if ru3:
+                tmpscore += 1
+                if ru1:
+                    ru1 = False
+                else:
+                    ru3 = False
+            else:
+                if ru1:
+                    ru3 = True
+                    ru1 = False
+            current += 1
+        elif arr[ining][nu[current % 9]] == 3:
+            if ru1:
+                tmpscore += 1
+                ru1 = False
+            if ru2:
+                tmpscore += 1
+                ru2 = False
+            if ru3:
+                tmpscore += 1
+            else:
+                ru3 = True
+            current += 1
+        elif arr[ining][nu[current % 9]] == 4:
+            if ru1:
+                tmpscore += 1
+                ru1 = False
+            if ru2:
+                tmpscore += 1
+                ru2 = False
+            if ru3:
+                tmpscore += 1
+                ru3 = False
+            tmpscore += 1
+            current += 1
+        if outpoint >= 3:
+            ining += 1
+            outpoint = 0
+            if ru1:
+                ru1 = False
+            if ru2:
+                ru2 = False
+            if ru3:
+                ru3 = False
+    if tmpscore > score:
+        score = tmpscore
 print(score)
