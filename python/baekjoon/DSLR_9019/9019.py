d3703510f76f9fbbf68d1f9727609f57b061c2f2
hello1334 <36934wkd@naver.com>
2023-09-13 23:24:23 +0900

    9-13

--- python/baekjoon/DSLR_9019/9019.py
+++ python/baekjoon/DSLR_9019/9019.py
@@ -2,6 +2,8 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
+from collections import deque
+
 def make_D(A):
     A = int(A)
     A = A*2
@@ -21,8 +23,15 @@ def make_S(A):
 
 def make_L(A):
     A = list(A)
-    A = A[1:] + A[0]
-    A = ''.join(A)
+    A = deque(A)
+    tmp = A.popleft()
+    A.append(tmp)
+    if len(A) > 1:
+        A = ''.join(A)
+    else:
+        A = A[0]
+    A = int(A)
+    A = str(A)
     return A
 
 
@@ -30,13 +39,41 @@ def make_R(A):
     A = list(A)
     tmp = [A.pop()]
     A = tmp + A
-    A = ''.join(A)
+    if len(A) > 1:
+        A = ''.join(A)
+    else:
+        A = A[0]
+    A = int(A)
+    A = str(A)
     return A
 
 
-def bfs(start):
-    visted = [0]*10001
-    
+def bfs(A):
+    visited = [0]*10002
+    q = [[A,'']]
+    visited[int(A)] = 1
+    q = deque(q)
+    while True:
+        i,j = q.popleft()
+        if i == B:
+            return j
+        tmp = j
+        if  visited[int((make_R(i)))] == 0:
+            j = tmp + 'R'
+            q.append((make_R(i),j))
+            visited[int((make_R(i)))] = 1
+        if  visited[int((make_L(i)))] == 0:
+            j = tmp + 'L'
+            q.append((make_L(i),j))
+            visited[int(make_L(i))] = 1
+        if  visited[int(make_S(i))] == 0:
+            j = tmp + 'S'
+            q.append((make_S(i),j))
+            visited[int(make_S(i))] = 1
+        if  visited[int((make_D(i)))] == 0:
+            j = tmp + 'D'
+            q.append((make_D(i),j))
+            visited[int(make_D(i))] = 1
 
 
 
@@ -44,5 +81,5 @@ def bfs(start):
 T = int(input())
 for tc in range(1, T+1):
     A, B = list(input().split())
-    print(A)
-    print(B)
+    ans = bfs(A)
+    print(ans)
