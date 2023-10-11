f46d2a00522320ce29ea42e5172423888e866fb9
전 건 휘 <36934wkd@naver.com>
2023-10-11 17:58:08 +0900

    10-11

--- DB_SQL/10-11/subject/db_hw_3_2/views.py
+++ DB_SQL/10-11/subject/db_hw_3_2/views.py
@@ -1,10 +1,11 @@
 # views.py
-def detail(request, __(a)__):
+def detail(request, pk):
     article = get_object_or_404(Article, pk=article_pk)
-
     comment_form = CommentForm()
+    comments = article.comment_set.all()
     context_data = {
         'article': article,
-        __(b)__,
+        'comment_form':comment_form,
+        'comments':comments,
     }
-    return render(request, 'eithers/detail.html', __(c)__)
+    return render(request, 'eithers/detail.html', context_data)
--- python/baekjoon/test.py
+++ python/baekjoon/test.py
@@ -1,40 +1,7 @@
-def per(start, end, k):
-    global percnt
-    if start == k:
-        percnt+=1
-        print(p)
-        return
-    for j in range(N):
-        if used[j] == 0:
-            p[start] = arr[j]
-            used[j] = 1
-            per(start+1, end, k)
-            used[j] = 0
-
-
-
-
-
-
-arr = [0,1,2,3,4,5,6,7,8,9]
-N = 10
-bitcnt = 0
-for i in range(1<<N):
-    group = []
-    # print(i)
-    for j in range(N):
-        # print(j)
-        if i & (1<<j):
-            # print(i & (1<<j))
-            group.append(arr[j])
-        bitcnt+= 1
-    if len(group) != 3:
+arr = [[[] for _ in range(3)] for _ in range(3)]
+arr[0][0].append(3)
+tmp = []
+while arr[0][0]:
+    i = arr[0][0].pop()
+    if groud[0][0] < i:
         continue
-
-print(bitcnt)
-
-percnt = 0
-p = [0]*N
-used = [0]*N
-per(0,N, 5)
-print(percnt)
