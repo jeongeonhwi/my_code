d101162a39b6ad2e7343677ded5ae70662f55830
hello1334 <36934wkd@naver.com>
2023-10-11 23:09:40 +0900

    10-11

--- python/baekjoon/mine_search_2140/2140.py
+++ python/baekjoon/mine_search_2140/2140.py
@@ -8,83 +8,50 @@ sys.stdin = open('input.txt', 'r')
 
 di = [1,-1,0,0,1,-1,1,-1]
 dj = [0,0,1,-1,1,1,-1,-1]
+ddi = [0,1,0,-1]
+ddj = [1,0,-1,0]
 N = int(input())
 arr = [list(input()) for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == '0' or arr[i][j] == '#' or arr[i][j] == '*':
-            continue
-        mine = 0
-        shyap = 0
-        for k in range(8):
-            ni,nj = i+di[k], j+dj[k]
-            if 0<=ni<N and 0<=nj<N:
-                if arr[ni][nj] == '#':
-                    shyap += 1
-                elif arr[ni][nj] == '*':
-                    mine += 1
-        if shyap == int(arr[i][j])-mine:
-            for k in range(8):
-                ni, nj = i+ di[k], j + dj[k]
-                if 0 <= ni < N and 0 <= nj < N:
-                    if arr[ni][nj] != '#':
-                        continue
-                    arr[ni][nj] = '*'
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == '0' or arr[i][j] == '#' or arr[i][j] == '*':
-            continue
-        mine = 0
-        one = False
-        oi, oj = 0,0
-        two = False
-        for k in range(8):
-            ni,nj = i+di[k], j+dj[k]
-            if 0<=ni<N and 0<=nj<N:
-                if arr[ni][nj] == '*':
-                    mine += 1
+si,sj = 1,1
+k = 0
+if N >= 3:
+    while True:
+        if k != 0 and si == 1 and sj == 1:
+            break
+        check = True
+        for kk in range(8):
+            ni,nj = si+di[kk], sj+dj[kk]
+            if arr[ni][nj] == '0':
+                check = False
+        if check:
+            for kk in range(8):
+                ni,nj = si+di[kk], sj+dj[kk]
+                if arr[ni][nj] == '0':
+                    continue
                 if arr[ni][nj] == '1':
-                    one = True
-                    oi,oj = ni,nj
-                if arr[ni][nj] == '2':
-                    two = True
-        if mine == int(arr[i][j]):
+                    arr[ni][nj] = '0'
+                    arr[si][sj] = '*'
+                elif arr[ni][nj] == '2':
+                    arr[ni][nj] = '1'
+                    arr[si][sj] = '*'
+                elif arr[ni][nj] == '3':
+                    arr[ni][nj] = '2'
+                    arr[si][sj] = '*'
+        si,sj = si+ddi[k], sj+ddj[k]
+        if 1<=si<N-1 and 1<=sj<N-1:
             continue
-        one = False
-        oi, oj = 0, 0
-        two = False
-        for k in range(8):
-            ni, nj = i + di[k], j + dj[k]
-            if 0 <= ni < N and 0 <= nj < N:
-                if arr[ni][nj] == '1':
-                    one = True
-                    oi, oj = ni, nj
-                if arr[ni][nj] == '2':
-                    two = True
-        if one:
-            if arr[i][j] == '1':
-                if i == 0:
-                    if arr[oi + 1][oj] != '#':
-                        continue
-                    arr[oi + 1][oj] = '*'
-                else:
-                    if arr[oi - 1][oj] != '#':
-                        continue
-                    arr[oi - 1][oj] = '*'
-                if j == 0:
-                    if arr[oi][oj + 1] != '#':
-                        continue
-                    arr[oi][oj + 1] = '*'
-                else:
-                    if arr[oi][oj - 1] != '#':
-                        continue
-                    arr[oi][oj - 1] = '*'
+        else:
+            si,sj = si-ddi[k], sj-ddj[k]
+            k+=1
 
 ans = 0
 for i in arr:
     for j in i:
         if j == '*':
             ans += 1
-for i in arr:
-    print(i)
-print(ans + (N-4)**2)
+if N > 3:
+    print(ans + (N-4)**2)
+elif N == 3:
+    print(ans)
+else:
+    print(0)
