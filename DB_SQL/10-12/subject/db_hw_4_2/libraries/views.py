9f6cc870c94c241565a658abb3f4301990712949
전 건 휘 <36934wkd@naver.com>
2023-10-12 17:13:03 +0900

    10-12

---
+++ DB_SQL/10-12/subject/db_hw_4_2/libraries/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect
+from .models import Book, Review
+
+# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    context = {
+        'books':books,
+    }
+    return render(request, 'libraries/index.html', context)
+
+
+def detail(request, book_pk):
+    book = 
