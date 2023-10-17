014e43a420fb4e168a409e63508b584f7853c0b2
hello1334 <36934wkd@naver.com>
2023-10-17 23:31:13 +0900

    10-17

--- python/baekjoon/test.py
+++ python/baekjoon/test.py
@@ -1,10 +1,10 @@
-N, M, H = map(int, input().split())
-arr = [[0]*(N+N) for _ in range(2*H+1)]
-for i in range(2*H+1):
-    for j in range(2*N):
-        if j%2==1:
-            arr[i][j] = 1
-for _ in range(M):
-    a,b = map(int, input().split())
-    arr[2*(a)-1][2*b] = 1
-
+a = '가나다라마바사아자차카타 #윤여정 #사랑해 #코미디'
+b = ''
+check = False
+for i in a:
+    if i == '#':
+        check = True
+    if check:
+        b+=i
+c = b.split(' ')
+print(c)
