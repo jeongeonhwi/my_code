583202432701b975b8c8c811a1e9c30057843c63
jgh <36934wkd@naver.com>
2024-01-04 17:57:06 +0900

    1-4

--- react/ReactTeck/ch1-start_react/hello-react/src/App.js
+++ react/ReactTeck/ch1-start_react/hello-react/src/App.js
@@ -1,9 +1,28 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const name = undefined;
-  return name || "값이 undefined입니다.";
+  const name = "리액트";
+  // const style = {
+  //   backgroundColor: "black",
+  //   color: "aqua",
+  //   fontSize: "48px",
+  //   fontWeight: "bold",
+  //   padding: 16,
+  // };
+
+  return (
+    <div>
+      {/* dfeffe */}
+      <form>
+        성: <br></br>
+        <input></input>
+        <br></br>
+        이름: <br></br>
+        <input></input>
+      </form>
+    </div>
+  );
 }
 
 export default App;
