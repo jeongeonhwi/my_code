432734dbfa7884e16d5e5aa4410ba332ee4efdbd
hello1334 <36934wkd@naver.com>
2024-01-27 11:11:08 +0900

    1-27

--- python/Algo/baekjoon/fast_train_1833/1833.py
+++ python/Algo/baekjoon/fast_train_1833/1833.py
@@ -6,29 +6,19 @@ sys.stdin = open('input.txt', 'r')
 # 10000을 넘지 않는 지연수는 설치 비용
 
 
-from heapq import heappop, heappush
 
-def daic(s):
-    visited = [(float('inf'),1,1)]*(N)
-    hq = [(0,s,-1)]
-    visited[s] = (0,s,-1)
-    while hq:
-        cnt,i, pi = heappop(hq)
-        print(i,hq)
-        if visited[i][0] < cnt:
-            continue
-        for ni in range(N):
-            next_cnt = cnt + arr[i][ni]
-            if visited[ni][0] <= next_cnt:
-                continue
-            visited[ni] = (next_cnt,pi,i)
-            heappush(hq, (next_cnt,ni, i))
-            print(next_cnt,ni,i)
-    return visited
+def find_set(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find_set(parent[x])
+    return parent[x]
 
+def union(x,y):
+    pass
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+parent = [i for i in range(N+1)]
 cost = 0
 for i in range(N):
     for j in range(N):
@@ -37,4 +27,4 @@ for i in range(N):
             arr[i][j] = 0
 cost //= 2
 
-print(daic(0))
+# print(daic(0))
--- python/Algo/baekjoon/test.py
+++ python/Algo/baekjoon/test.py
@@ -1,5 +1,51 @@
-a = [(1,(1,1)), (2,(2,2))]
-from heapq import heappop, heappush
+from heapq import heappush, heappop
 
-heappush(a, (3,(3,3)))
-print(a)
+def find_s_e(board):
+    s,e = 0,0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'R':
+                s = (i,j)
+            if board[i][j] == 'G':
+                e = (i,j)
+    return (s,e)
+
+def bfs(si,sj,N,M,board):
+    direction = {1:(0,1), 2:(0,-1), 3:(1,0), 4:(-1,0)}
+    visited = [[float('inf')]*M for _ in range(N)]
+    q = [(0,si,sj)]
+    visited[si][sj] = 0
+    while q:
+        c,i,j = heappop(q)
+        print(c,i,j,board[i][j])
+        if visited[i][j] < c:
+            continue
+        for d in [(0,1),(0,-1),(1,0),(-1,0)]:
+            ni,nj = i+d[0], j+d[1]
+            while True:
+                if 0<=ni<N and 0<=nj<M:
+                    if board[ni][nj] == 'D':
+                        if board[ni-d[0]][nj-d[1]] == 'G':
+                            return c+1
+                        if visited[ni-d[0]][nj-d[1]] <= c+1:
+                            break
+                        visited[ni-d[0]][nj-d[1]] = c+1
+                        heappush(q, (c+1,ni-d[0],nj-d[1]))
+                        break
+                else:
+                    if visited[ni-d[0]][nj-d[1]] <= c+1:
+                        break
+                    visited[ni-d[0]][nj-d[1]] = c+1
+                    heappush(q, (c+1,ni-d[0],nj-d[1]))
+                    break
+                ni,nj = ni+d[0], nj+d[1]
+
+    return -1
+
+board = ['...','..R','..G']
+answer = 0
+N = len(board)
+M = len(board[0])
+start, end = find_s_e(board)
+answer = bfs(start[0],start[1],N,M,board)
+print(answer)
