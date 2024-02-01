5123babd28a4a755c13e93d5dabdfd3e9a15040b
hello1334 <36934wkd@naver.com>
2024-02-01 23:54:30 +0900

    2-1

--- python/Algo/baekjoon/test.py
+++ python/Algo/baekjoon/test.py
@@ -1,12 +1,18 @@
-n,k = map(int, input().split())
-arr = input().strip()
-arr *= k
-ans = 'NO'
-d = {U:(-1,0), L:(0,-1), R:(0,1), D:(1,0)}
-si,sj = d[arr[0]]
-for i in range(1, len(arr)):
-    si,sj = si+d[arr[i]][0], sj+d[arr[i]][1]
-    if si == 0 and sj == 0:
-        ans = 'YES'
-        break
+import sys
+input = sys.stdin.readline
+
+
+from heapq import heappop, heappush
+n = int(input())
+card = [int(input()) for _ in range(n)]
+card.sort()
+ans = 0
+if len(card) > 1:
+    while True:
+        if len(card) == 2:
+            ans += card[0]+card[1]
+            break
+        a,b = heappop(card), heappop(card)
+        ans += (a+b)
+        heappush(card, (a+b))
 print(ans)
