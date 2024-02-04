0b8de835cbd6222ac40ecf1c5cc3522fc4228675
hello1334 <36934wkd@naver.com>
2024-02-04 23:27:51 +0900

    2-4

--- python/Algo/baekjoon/test.py
+++ python/Algo/baekjoon/test.py
@@ -1,18 +1,6 @@
-import sys
-input = sys.stdin.readline
-
-
-from heapq import heappop, heappush
-n = int(input())
-card = [int(input()) for _ in range(n)]
-card.sort()
-ans = 0
-if len(card) > 1:
-    while True:
-        if len(card) == 2:
-            ans += card[0]+card[1]
-            break
-        a,b = heappop(card), heappop(card)
-        ans += (a+b)
-        heappush(card, (a+b))
-print(ans)
+n = list(input())
+for i in range(len(n)):
+    n[i] = int(n[i])
+n.sort(reverse=True)
+for i in n:
+    print(i, end='')
