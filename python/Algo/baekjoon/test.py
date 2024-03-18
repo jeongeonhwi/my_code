aca165aec9429b0b811e5c91247b20eae4dd8c68
hello1334 <36934wkd@naver.com>
2024-03-19 00:14:14 +0900

    3-19

--- python/Algo/baekjoon/test.py
+++ python/Algo/baekjoon/test.py
@@ -1,15 +1,8 @@
-from stable_diffusion_tf import StableDiffusion
-import matplotlib.pyplot as plt
+a = ['a','b']
+b = ['a', 'c']
+c = ['a','b']
 
-# 모델 초기화
-model = StableDiffusion.from_pretrained('CompVis/stable-diffusion-v1-4')
-
-# 이미지 생성을 위한 프롬프트
-prompt = "이 사진을 디즈니 스타일의 카툰으로 변환해주세요"
-
-# 이미지 생성
-generated_images = model.generate(prompt)
-
-# 생성된 이미지 시각화
-plt.imshow(generated_images[0])
-plt.show()
+if a == c:
+    print('gggg')
+else:
+    print('bbbbb')
