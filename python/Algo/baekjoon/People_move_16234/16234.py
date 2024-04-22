5dc8cb3bee8c76aca967fe2ec31568350c98200d
hello1334 <36934wkd@naver.com>
2024-04-23 01:19:12 +0900

    백준 인구이동 솔

--- python/Algo/baekjoon/People_move_16234/16234.py
+++ python/Algo/baekjoon/People_move_16234/16234.py
@@ -1,29 +1,46 @@
+import copy
 import sys
 sys.stdin = open('input.txt', 'r')
 # input = sys.stdin.readline
 
+def dfs(si,sj):
+    stack = [(si,sj)]
+    tmp_visited[si][sj] = [(si,sj)]
+    while stack:
+        i,j = stack.pop()
+        for ni,nj in [(i+1,j),(i,j+1),(i-1,j),(i,j-1),]:
+            if 0<=ni<N and 0<=nj<N and tmp_visited[ni][nj] == 0:
+                if L<=abs(arr[ni][nj] - arr[i][j])<=R:
+                    tmp_visited[ni][nj] = 1
+                    tmp_visited[si][sj].append((ni,nj))
+                    stack.append((ni,nj))
+
+
 N,L,R = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-save = []
+visited = [[0]*N for _ in range(N)]
 ans = 0
 while True:
+    tmp_visited = copy.deepcopy(visited)
     for i in range(N):
         for j in range(N):
-            for ni, nj in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
-                if 0<=ni<N and 0<=nj<N:
-                    if L<= abs(arr[i][j] - arr[ni][nj]) <= R:
-                        save.add((ni,nj))
-                        save.add((i,j))
-    if len(save) == 0:
+            if tmp_visited[i][j] == 0:
+                dfs(i,j)
+    flag = 0
+    for i in range(N):
+        for j in range(N):
+            if type(tmp_visited[i][j]) == list and len(tmp_visited[i][j]) > 1:
+                sumdata = 0
+                for ti,tj in tmp_visited[i][j]:
+                    sumdata += arr[ti][tj]
+                for ti,tj in tmp_visited[i][j]:
+                    arr[ti][tj] = sumdata//len(tmp_visited[i][j])
+            else:
+                flag += 1
+    if flag == N*N:
         break
+
     ans += 1
-    sumdata = 0
-    for si,sj in save:
-        sumdata += arr[si][sj]
-    chageData = sumdata // len(save)
-    for si,sj in save:
-        arr[si][sj] = chageData
-    save.clear()
 
 print(ans)
