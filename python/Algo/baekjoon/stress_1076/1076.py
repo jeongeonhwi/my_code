acb9d20ad88cf8b9409aa764bee4f91330e35887
hello1334 <36934wkd@naver.com>
2024-08-04 10:03:06 +0900

    python algo study

---
+++ python/Algo/baekjoon/stress_1076/1076.py
@@ -0,0 +1,5 @@
+a = input()
+b = input()
+c = input()
+stress = {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'violet':7,'grey':8,'white':9,}
+print((stress[a]*10+stress[b])*10**stress[c])
