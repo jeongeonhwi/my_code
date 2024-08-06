c969ba704c1b7f2973024ae57846418308f17aa7
jeongeonhwi <36934wkd@naver.com>
2024-08-06 12:42:13 +0900

    java algo baekjoon 10026

--- java/Algo/baekjoon/algo/rgcolor_10026.java
+++ java/Algo/baekjoon/algo/rgcolor_10026.java
@@ -3,8 +3,51 @@ import java.lang.*;
 import java.io.*;
 
 public class rgcolor_10026 {
-    public int dfs() {
-        return 0;
+    static int n = 0, ncnt=0, ccnt=0;
+    static int[][] ncvisited = new int[n][n];
+    static int[][] visited = new int[n][n];
+
+    public static void dfs(char c, int i, int j, String[]arr) {
+        int[] di = {-1,1,0,0};
+        int[] dj = {0,0,-1,1};
+
+        visited[i][j] = 1;
+
+        for (int d = 0; d<4; d++) {
+            int ni = i+di[d];
+            int nj = j+dj[d];
+
+            if (ni>=0 && nj>=0 && ni<n && nj<n) {
+                if (visited[ni][nj] == 0 && arr[ni].charAt(nj) == c) {
+                    dfs(c,ni,nj,arr);
+                }
+            }
+        }
+    }
+
+    public static void ndfs(char c, int i, int j, String[]arr) {
+        int[] di = {-1,1,0,0};
+        int[] dj = {0,0,-1,1};
+
+        ncvisited[i][j] = 1;
+
+        for (int d = 0; d<4; d++) {
+            int ni = i+di[d];
+            int nj = j+dj[d];
+
+            if (ni>=0 && nj>=0 && ni<n && nj<n) {
+                if (c == 'R' || c == 'G') {
+                    if (ncvisited[ni][nj] == 0 && (arr[ni].charAt(nj) == 'R' || arr[ni].charAt(nj) == 'G')) {
+                        ndfs(c,ni,nj,arr);
+                    }
+                }
+                else  {
+                    if (ncvisited[ni][nj] == 0 && arr[ni].charAt(nj) == c) {
+                        ndfs(c,ni,nj,arr);
+                        }
+                    }
+            }
+        }
     }
 
     public static void main (String[] arg) throws IOException  {
@@ -14,15 +57,33 @@ public class rgcolor_10026 {
         String inputn = br.readLine();
         StringTokenizer st = new StringTokenizer(inputn);
 
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        ncvisited = new int[n][n];
+        visited = new int[n][n];
 
         String[] arr = new String[n];
-        int[][] ncvisited = new int[n][n];
-        int[][] visited = new int[n][n];
+
 
         for (int i = 0; i<n; i++) {
             String inputline = br.readLine();
             arr[i] = inputline;
         }
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<n; j++) {
+                if (visited[i][j] == 0) {
+                    dfs(arr[i].charAt(j), i ,j, arr);
+                    ccnt++;
+                }
+                if (ncvisited[i][j] == 0) {
+                    ndfs(arr[i].charAt(j), i,j, arr);
+                    ncnt++;
+                }
+            }
+        }
+        System.out.print(ccnt);
+        System.out.print(' ');
+        System.out.print(ncnt);
     }
 }
