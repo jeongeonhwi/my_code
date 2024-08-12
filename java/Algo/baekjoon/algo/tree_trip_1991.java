fd4f7fff626fd118d813db11cb9d7856f6307c39
hello1334 <36934wkd@naver.com>
2024-08-12 20:17:41 +0900

    java algo 1991 study

--- java/Algo/baekjoon/algo/tree_trip_1991.java
+++ java/Algo/baekjoon/algo/tree_trip_1991.java
@@ -3,24 +3,68 @@ import java.lang.*;
 import java.io.*;
 
 public class tree_trip_1991 {
-    static int[] tree;
+    static int[] parent_tree;
+    static int[][] child_tree;
+    static String pre="", in="", post="";
+
+    public static void preorder(int node) {
+        pre += (char) node;
+        for (int i=0; i<2; i++) {
+            if (child_tree[node][i] != 0) {
+                preorder(child_tree[node][i]);
+            }
+        }
+    }
+
+    public static void inorder(int node) {
+        if (child_tree[node][0] != 0) {
+            inorder(child_tree[node][0]);
+        }
+        in += (char) node;
+        if (child_tree[node][1] != 0) {
+            inorder(child_tree[node][1]);
+        }
+    }
+
+    public static void postorder(int node) {
+        for (int i=0; i<2; i++) {
+            if (child_tree[node][i] != 0) {
+                postorder(child_tree[node][i]);
+            }
+        }
+        post += (char) node;
+    }
+
     public static void main(String [] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
 
         String inputline = br.readLine();
         StringTokenizer st = new StringTokenizer(inputline);
 
-        tree = new int[100];
+        parent_tree = new int[100];
 
-        tree[65] = 65;
+        child_tree = new int[100][2];
+
+        parent_tree[65] = 65;
 
         int node_number = Integer.parseInt(st.nextToken());
 
         for (int i=0; i<node_number; i++) {
             inputline = br.readLine();
-            String[] data = inputline.split(" ");
-//            int a = (int) data[1];
+            int a = inputline.charAt(0);
+            char charb = inputline.charAt(2);
+            if (charb != '.') {
+                parent_tree[charb] = a;
+                child_tree[a][0] = charb;
+            }
+            char charc = inputline.charAt(4);
+            if (charc != '.') {
+                parent_tree[charc] = a;
+                child_tree[a][1] = charc;
+            }
         }
+
+        preorder(65); inorder(65); postorder(65);
+        System.out.println(pre + '\n' + in + '\n' + post);
     }
 }
