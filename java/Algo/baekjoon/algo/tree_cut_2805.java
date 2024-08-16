bbaf698a9b8a880e6e7f58d5f6187a779b2f8121
jgh <36934wkd@naver.com>
2024-08-16 16:22:30 +0900

    java algo 2805

--- java/Algo/baekjoon/algo/tree_cut_2805.java
+++ java/Algo/baekjoon/algo/tree_cut_2805.java
@@ -3,31 +3,31 @@ import java.lang.*;
 import java.util.*;
 
 public class tree_cut_2805 {
-    static double N,M, now = 0, tree_count = 0, high=0;
-    static double[] nlist;
-    static ArrayDeque<Double> deque;
+    static int N,M, now = 0, tree_count = 0, high=0;
+    static int[] nlist;
+    static ArrayDeque<Integer> deque;
     public static void main (String [] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputline = br.readLine();
         StringTokenizer st = new StringTokenizer(inputline);
-        N = Double.parseDouble(st.nextToken()); M = Double.parseDouble(st.nextToken()); nlist = new double[N]; deque = new ArrayDeque<>();
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken()); nlist = new int[N]; deque = new ArrayDeque<>();
         inputline = br.readLine();
         st = new StringTokenizer(inputline);
-        for (double i=0; i<N; i++) {
-            nlist[i] = Double.parseDouble(st.nextToken());
+        for (int i=0; i<N; i++) {
+            nlist[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nlist); high = nlist[N-1];
-        for (double i: nlist) deque.add(i);
+        for (int i: nlist) deque.add(i);
         while (!deque.isEmpty()) {
             // 위에서부터 벌목할 나무를 계산
-            double toptree = deque.removeLast();
+            int toptree = deque.removeLast();
             tree_count += 1;
             while (!deque.isEmpty()) {
                 if (deque.peekLast() == toptree) {
                     deque.removeLast(); tree_count += 1;
                 } else break;
             }
-            double secondtree = toptree;
+            int secondtree = toptree;
             if (!deque.isEmpty()) secondtree = deque.peekLast();
             if (secondtree == toptree) {
                 if (M%tree_count == 0) {
@@ -55,7 +55,6 @@ public class tree_cut_2805 {
                 now += (toptree-secondtree)*tree_count;
                 high = secondtree;
             }
-            System.out.println(M+" "+now+" "+tree_count+" "+high+" "+toptree+" "+secondtree);
         }
         System.out.println(high);
     }
