f21a0f48b2c7a00af7668f90623472dd7658b6fe
jgh <36934wkd@naver.com>
2024-08-21 10:12:15 +0900

    java algo 1167

--- java/Algo/baekjoon/algo/color_paper_2630.java
+++ java/Algo/baekjoon/algo/color_paper_2630.java
@@ -8,7 +8,7 @@ public class color_paper_2630 {
 
     public static void find_color_paper(int n, int si,int sj) {
         if (n == 1) {
-            System.out.println(arr[si][sj]+" "+si+" "+sj+" "+si+n+" "+sj+n);
+//            System.out.println(arr[si][sj]+" "+si+" "+sj+" "+n);
             if (arr[si][sj] == 0) white_cnt++;
             else blue_cnt++;
             return;
@@ -20,7 +20,7 @@ public class color_paper_2630 {
             return;
         }
         find_color_paper(n/2, si,sj);
-        find_color_paper(n/2, si,si+n/2);
+        find_color_paper(n/2, si,sj+n/2);
         find_color_paper(n/2, si+n/2,sj);
         find_color_paper(n/2, si+n/2,sj+n/2);
     }
@@ -35,6 +35,7 @@ public class color_paper_2630 {
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
         String inputline = br.readLine();
         StringTokenizer st = new StringTokenizer(inputline);
         N = Integer.parseInt(st.nextToken()); arr = new int[N][N]; visited = new int[N][N];
@@ -46,6 +47,8 @@ public class color_paper_2630 {
             }
         }
         find_color_paper(N, 0,0);
-        System.out.println(white_cnt+"\n"+blue_cnt);
+//        System.out.println(white_cnt+"\n"+blue_cnt);
+        pw.write(white_cnt+"\n"+blue_cnt);
+        pw.flush();
     }
 }
