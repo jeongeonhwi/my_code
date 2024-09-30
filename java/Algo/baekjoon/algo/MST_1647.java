0a5c764a9d3e9df01d36775ee05ec8820343ea49
jgh <36934wkd@naver.com>
2024-09-30 16:06:57 +0900

    java MST algo 1647

--- java/Algo/baekjoon/algo/MST_1647.java
+++ java/Algo/baekjoon/algo/MST_1647.java
@@ -3,6 +3,8 @@ import java.lang.*;
 import java.util.*;
 
 public class MST_1647 {
+    static int N,M;
+    static int[][] arr;
     public static void main (String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
@@ -10,5 +12,16 @@ public class MST_1647 {
         String currentDir = System.getProperty("user.dir");
         String filePath = currentDir + "/Algo/baekjoon/algo/input/1647.txt";
         BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+        String inputline = br.readLine();
+        StringTokenizer st = new StringTokenizer(inputline);
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1][1];
+
+        for (int i=0; i<M; i++) {
+            inputline = br.readLine();
+            st = new StringTokenizer(inputline);
+        }
     }
 }
