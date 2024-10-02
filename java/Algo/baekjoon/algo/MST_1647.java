21e38b7ce51f253d78ea2491156af16bc9c9aa5d
jgh <36934wkd@naver.com>
2024-10-02 14:32:19 +0900

    java MST algo 1647

--- java/Algo/baekjoon/algo/MST_1647.java
+++ java/Algo/baekjoon/algo/MST_1647.java
@@ -2,9 +2,52 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
+class Edge implements Comparable<Edge> {
+    int w;
+    int cost;
+    Edge(int w, int cost) {
+        this.w = w;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
+    }
+}
+
 public class MST_1647 {
     static int N,M;
-    static int[][] arr;
+    static List<Edge>[] arr;
+
+    public static int prim(int start) {
+        boolean[] visit = new boolean[N+1];
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+
+        int total = 0;
+
+        while(!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int v = edge.w;
+            int cost = edge.cost;
+
+            if (visit[v]) continue;
+//            System.out.print(v);
+//            System.out.println(cost);
+            visit[v] = true;
+            total += cost;
+
+            for(Edge e : arr[v]) {
+                if(!visit[e.w]) {
+                    pq.add(e);
+                }
+            }
+        }
+        return total;
+    }
+
     public static void main (String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
@@ -17,11 +60,22 @@ public class MST_1647 {
         StringTokenizer st = new StringTokenizer(inputline);
         N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+1][1];
+        arr = new List[N+1];
+        for (int i = 0; i < arr.length; i++) arr[i] = new ArrayList<>();
 
         for (int i=0; i<M; i++) {
             inputline = br.readLine();
             st = new StringTokenizer(inputline);
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            arr[v].add(new Edge(w, cost));
+            arr[w].add(new Edge(v, cost));
         }
+
+        int min_v = prim(1);
+        for (int i = 2; i<N+1; i++) min_v = Math.min(min_v, prim(i));
+//        System.out.println(min_v);
     }
 }
