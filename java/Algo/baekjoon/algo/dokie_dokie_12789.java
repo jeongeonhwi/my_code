93c4c2461ac36bc9dd66146bcda1817ca6191e91
jgh <36934wkd@naver.com>
2024-10-10 18:14:56 +0900

    java deque algo 12789

--- java/Algo/baekjoon/algo/dokie_dokie_12789.java
+++ java/Algo/baekjoon/algo/dokie_dokie_12789.java
@@ -3,8 +3,8 @@ import java.lang.*;
 import java.util.*;
 
 public class dokie_dokie_12789 {
-    static int N;
-    static ArrayDeque<Integer> arr = new ArrayDeque<>();
+    static int N, now = 1;
+    static ArrayDeque<Integer> arr = new ArrayDeque<>(), side = new ArrayDeque<>();
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
@@ -18,8 +18,32 @@ public class dokie_dokie_12789 {
         st = new StringTokenizer(inputline);
 
         for (int i=0; i<N; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            arr.add(a);
+            int c = Integer.parseInt(st.nextToken());
+            side.addLast(c);
         }
+
+        boolean flag = true;
+
+        for (int i=0; i<N; i++) {
+            int a = side.removeFirst();
+            if (now == a) now++;
+            else if (!arr.isEmpty() && arr.peekLast() == now) {
+                arr.removeLast(); now++;
+                side.addFirst(a);
+            }
+            else {
+                if (arr.isEmpty()) arr.add(a);
+                else {
+                    if (arr.peekLast() < a) {
+                        flag = false;
+                        break;
+                    }
+                    else arr.add(a);
+                }
+            }
+        }
+        if (flag) pw.println("Nice");
+        else pw.println("Sad");
+        pw.flush();
     }
 }
