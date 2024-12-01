879a5c7a0f1992a5cedebe70b14a788cc85af5e6
전건휘 <36934wkd@naver.com>
2024-12-01 17:10:39 +0900

    python grid algo 1946

--- python/Algo/baekjoon/new_co_1946/1946.py
+++ python/Algo/baekjoon/new_co_1946/1946.py
@@ -5,20 +5,12 @@ sys.stdin = open('input.txt', 'r')
 T = int(input())
 for _ in range(T):
     N = int(input())
-    mi, mj = 100001, 100001
-    for _ in range(N):
-        a,b = map(int, input().split())
-        stack = []
-        if a < mi and b < mj:
-            mi,mj = a,b
-            tmp = []
-            while stack:
-                i,j = stack.pop()
-                if mi < i and mj < j:
-                    continue
-                tmp.append((i,j))
-            stack = tmp
-        else:
-            if a <= mi or b <= mj:
-                stack.append((a,b))
-    print(len(stack))
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr.sort()
+    top = 0
+    ans = 1
+    for i in range(1,N):
+        if arr[i][1] < arr[top][1]:
+            ans += 1
+            top = i
+    print(ans)
